b2d0574d03138fd6fb7cddcee502180286b1c892
atharva22062206 <115336349+atharva22062206@users.noreply.github.com>
2023-01-29 15:45:25 +0530

    Update script.js

--- javasc/script.js
+++ javasc/script.js
@@ -4,7 +4,7 @@ selectMenu = document.querySelectorAll("select"),
 setAlarmBtn = document.querySelector("button");
 
 let alarmTime, isAlarmSet,
-ringtone = new Audio("music/bonespaglasongs_BMeuFKa9.mp3");
+ringtone = new Audio(""./files/music/bonespaglasongs_BMeuFKa9.mp3");
 
 for (let i = 12; i > 0; i--) {
     i = i < 10 ? `0${i}` : i;
